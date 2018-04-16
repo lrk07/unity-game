4d45899cb89c7df049b4ec170eeeaea377f16a32
lrk07 <mateusz.ligara@gmail.com>
2018-04-16 21:28:42 +0200

    add start gui

---
+++ Assets/Script/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+	public GameObject StartUI;
+	public static bool isGameStarted = false;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update() {
+		if (isGameStarted) {
+			StartUI.SetActive (false);
+		}
+		if (!isGameStarted) {
+			StartUI.SetActive (true);
+		}
+	}
+
+	public void StartPanelHide() {
+		isGameStarted = true;
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
+
+
